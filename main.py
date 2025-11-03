4372b7dec86d019ac72855bc1b4afe7ec8dd3687
Natalia Sanabria <sanabria.forero.natalia@gmail.com>
2025-11-02 23:23:41 -0500

    Added the player class and basic logic for drawing

--- main.py
+++ main.py
@@ -4,19 +4,27 @@ from constants import (SCREEN_WIDTH,
                        ASTEROID_KINDS,
                        ASTEROID_SPAWN_RATE,
                        ASTEROID_MAX_RADIUS)
+from player import Player
 import pygame
 
 
 def main():
     pygame.init()
+
+    clock = pygame.time.Clock()
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
-
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
