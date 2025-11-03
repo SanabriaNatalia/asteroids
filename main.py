06e4fa2fe7d2e58ad335769bc174b2a2f5d39fd5
Natalia Sanabria <sanabria.forero.natalia@gmail.com>
2025-11-02 23:11:32 -0500

    Fixed color bug

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill('0,0,0,1')
+        screen.fill("black")
         pygame.display.flip()
 
 
